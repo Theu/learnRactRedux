29f860ad88cd9f0a6f6f177e7170bf20f969d0a2
Theu <info@matteocontini.com>
2017-01-14 15:57:32 +0100

    CH04.2 creata App.js con list di utenti. Usato map e push

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hallo world!</h1>, document.getElementById('app'));
+import App from './components/App';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
