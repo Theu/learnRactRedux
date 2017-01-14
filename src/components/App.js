d8a294e2b0e250a5570b5587867db9a7c08545e6
Theu <info@matteocontini.com>
2017-01-14 17:03:15 +0100

    CH04.03 created component userList, userListElements. Created props

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import UserList from './UserList';
 /*
   App Component
 */
@@ -20,36 +22,7 @@ export default class App extends React.Component {
   }
   render() {
     return(
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>UserName</th>
-            <th>Job</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.users.map((user, index) => {
-            return(
-              <tr key={user.id}>
-                <td>#{user.id}</td>
-                <td>{user.username}</td>
-                <td>{user.job}</td>
-                <td>
-                  <a href={'/user-edit/' + user.id}>
-                    Edit
-                  </a>
-                </td>
-                <td>
-                  <button data-id={user.id}>Delete</button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <UserList users={this.state.users}/>
     );
   }
 }
